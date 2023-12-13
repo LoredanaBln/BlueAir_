7d6fc35c58a381d01e18edf58748c9c1ea6a0656
Loredana Balan <15789abcd@gmail.com>
2023-12-13 18:31:27 +0200

    all

---
+++ app/src/main/java/com/project/ForgotPasswordTask.java
@@ -0,0 +1,14 @@
+package com.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_task);
+    }
+}
