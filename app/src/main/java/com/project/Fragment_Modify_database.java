6d551e8e50d8623672f995239b6f62a63f2754b0
Loredana Balan <15789abcd@gmail.com>
2023-12-22 20:26:59 +0200

    mda

--- app/src/main/java/com/project/Fragment_Modify_database.java
+++ app/src/main/java/com/project/Fragment_Modify_database.java
@@ -1,12 +1,15 @@
 package com.project;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,51 +23,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_Modify_database#newInstance} factory method to
- * create an instance of this fragment.
+
  */
 public class Fragment_Modify_database extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private static final String url = "jdbc:mysql://" + DBConnectionCredentials.ip + "/" + DBConnectionCredentials.databaseName +"?characterEncoding=latin1&autoReconnect=true&useSSL=false";
+    private static final String url = "jdbc:mysql://" + DBConnectionCredentials.ip + "/" + DBConnectionCredentials.databaseName + "?characterEncoding=latin1&autoReconnect=true&useSSL=false";
     private static final String user = DBConnectionCredentials.username;
     private static final String pass = DBConnectionCredentials.password;
 
     RecyclerView recyclerView;
+    //COMMENT====== INITIALIZE VARIABLES FOR QUERY =====//
+    private String stringDepartLocation, stringArriveLocation, stringDateDepart, stringFlyingClass, stringFlyingClassTicket;
     List<Ticket> listOfTickets;
-    Adapter adapter;
     public Fragment_Modify_database() {
         listOfTickets = new ArrayList<>();
     }
-
-    public static Fragment_Modify_database newInstance(String param1, String param2) {
-        Fragment_Modify_database fragment = new Fragment_Modify_database();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.recycler_main);
+        Fragment_Modify_database.ConnectMySql connectMySql = new Fragment_Modify_database.ConnectMySql();
+        connectMySql.execute("");
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment__modify_database, container, false);
     }
 
-    public void displayTickets(){
+    public void displayTickets() {
 
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),1));
-
-        adapter = new Adapter(getActivity(), listOfTickets);
+        Adapter adapter = new Adapter(getActivity(), listOfTickets);
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+
+
     }
     private class ConnectMySql extends AsyncTask<String, Void, String> {
         String res = "";
@@ -79,25 +78,38 @@ public class Fragment_Modify_database extends Fragment {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection con = DriverManager.getConnection(url, user, pass);
-                System.out.println("Databaseection success");
+                System.out.println("Connection success");
 
                 String result = "";
                 Statement st = con.createStatement();
-                // SELECTS ALL THE ITEMS IN THE USERS CART
-                ResultSet rs = st.executeQuery("SELECT flights.* FROM flights");
+                //COMMENT SELECTS ALL THE FLIGHTS WITH PARAMETERS FROM CONSTRUCTOR
+                String query = String.format("SELECT flights.*, airlines.AirlineName FROM flights JOIN airlines ON flights.AirlineID = airlines.AirlineID WHERE flights.DepartureLocationCountry = \"%s\" and flights.ArrivalLocationCountry = \"%s\" and flights.DepartureTime like \"%%%s%%\" and %s >0", stringDepartLocation, stringArriveLocation, stringDateDepart, stringFlyingClassTicket);
+                Log.i("QUERY", query);
+                ResultSet rs = st.executeQuery(query);
                 ResultSetMetaData rsmd = rs.getMetaData();
 
-                // FOR EACH ITEM SEARCH FOR THE FLIGHT AND ADD IT IN THE LIST
+                //COMMENT FOR EACH ITEM SEARCH FOR THE FLIGHT AND ADD IT IN THE LIST
                 while (rs.next()) {
+                    int flightID = rs.getInt(1);
+                    Log.i("query", Integer.toString(flightID));
+                    // ORAS DEPART
                     String depart = rs.getString(3);
+                    // ORAS ARRIVE
                     String arrive = rs.getString(5);
+                    // obvious
                     String dateDepart = rs.getString(7);
                     String dateArrive = rs.getString(8);
-                    listOfTickets.add(new Ticket(depart,arrive, dateDepart, dateDepart, "3h", "Wizz", "30"));
+                    // TICKET PRICES
+                    String ticketPrice;
+                    if(stringFlyingClass.equals("Economy"))
+                        ticketPrice = rs.getString(18);
+                    else if(stringFlyingClass.equals("Business"))
+                        ticketPrice = rs.getString(17);
+                    else
+                        ticketPrice = rs.getString(16);
+                    String company = rs.getString(20);
 
-                    dateDepart = rs.getString(7).replaceAll("\\s.*", "");
-                    dateArrive = rs.getString(8).replaceAll("\\s.*", "");
-                    listOfTickets.add(new Ticket(depart,arrive, dateDepart, dateArrive, "3h", "Wizz", "22"));
+                    listOfTickets.add(new Ticket(depart,arrive, dateDepart, dateArrive, stringFlyingClass, company, ticketPrice, flightID, Activity_login.accountName));
                 }
                 res = result;
             } catch (Exception e) {
