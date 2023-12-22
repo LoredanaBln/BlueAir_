d9678ff657de51427d8a3b79cd00c499709146ca
Devwa <emericbartha@gmail.com>
2023-12-22 13:55:36 +0200

    cart updates on item delete

--- app/src/main/java/com/project/DBConnectionCredentials.java
+++ app/src/main/java/com/project/DBConnectionCredentials.java
@@ -7,7 +7,7 @@ public class DBConnectionCredentials {
     public static String password = "octombrie14";
     public static String databaseName = "project";
 
-   // public static String ip = "192.168.201.48:3306";
+    //public static String ip = "192.168.201.48:3306";
     ///public static String username = "admin";
     //public static String password = "ianuarie31";
     //public static String databaseName = "project";
